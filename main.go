799de7eea3dc2b3847967ae3b88c5373244b41c5
lemonit_cn <liuri@lemonit.cn>
2018-12-17 09:56:21 +0800

    a

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 	"fmt"
 	"lemon-robot-dispatcher/structs"
 	"os/exec"
-	"strconv"
-	"time"
+	"runtime"
 )
 
 const executorPath = "/Users/lemonit_cn/Documents/code/lemon-robot/lemon-robot-runner-executor/target/lemon-robot-runner-executor-0.0.1-jar-with-dependencies.jar"
@@ -19,20 +18,23 @@ var (
 )
 
 func main(){
-	runningPool = make(map[string] structs.Running)
-	runningKey := strconv.FormatInt(time.Now().Unix(), 10)
-	go executeInstance(runningKey)
-	fmt.Println("OVER")
-	time.Sleep(20  * time.Second)
-	runningPool[runningKey].CancelFunc()
-	fmt.Println("OVER222")
+	fmt.Println(runtime.GOARCH)
+	fmt.Printlake(map[string] structs.Running)
+	//runningKey := strconv.FormatInt(time.Now().Unix(), 10)
+	//go executeInstance(runningKey)
+	//fmt.Println("OVER")
+	//time.Sleep(20  * time.Second)
 	//runningPool[runningKey].CancelFunc()
-	//time.Sleep(2 * time.Second)
-	logs := runningPool[runningKey].Logs
-	for i := logs.Front(); i != nil; i = i.Next() {
-		fmt.Println(logs.Len(), "ja:", i.Value)
-		i.Next()
-	}
+	//fmt.Println("OVER222")
+	////runningPool[runningKey].CancelFunc()
+	////time.Sleep(2 * time.Second)
+	//logs := runningPool[runningKey].Logs
+	//for i := logs.Front(); i != nil; i = i.Next() {
+	//	fmt.Println(logs.Len(), "ja:", i.Value)
+	//	i.Next()
+	//}n(runtime.GOOS)
+	fmt.Println("=================")
+	//runningPool = m
 }
 
 func executeInstance(runningKey string) {
